68b36164c8d0fa6aa701d1abec66edfa1a025a3b
Wenting <55959544+WentingMiao@users.noreply.github.com>
2020-12-11 15:25:18 +0100

    add the test file of anonymousUser login

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory
+from .views import *
+class loginTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_login_anoymousUser(self):
+      request = self.factory.get("/accounts/login")
+      request.user = AnonymousUser()
+      response = account_login(request)
+      self.assertEqual(response.status_code, 200)
+
